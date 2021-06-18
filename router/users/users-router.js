88480ccbdf38a9d20dc2e71ee303f0c9299a0a59
Liliana Leyva <ingeleyva@yahoo.com>
2021-06-18 00:03:35 -0500

    login endpoint

--- router/users/users-router.js
+++ router/users/users-router.js
@@ -1,24 +1,57 @@
+require('dotenv').config()
 const express = require("express")
 const router = express.Router()
+const jwt = require('jsonwebtoken')
 
-
-let users = [
-	{ id: "1", name: "Jane Doe" },
-	{ id: "2", name: "John Doe" },
-	{ id: "3", name: "Jack Doe" },
+const posts = [
+	{
+		username: 'Lily',
+		title: 'Post 1',
+	},
+	{
+		username: 'Arely',
+		title: 'Post 2',
+	},
 ]
-
 router.get("/", (req, res) => {
 	res.status(200).json({
 		message: "Welcome to Pintereach-2"
 	})
 })
 
-router.get("/users", (req, res) => {
-	res.status(200).json(users)
+router.get('/api/posts' ,  authenticateToken,(req, res) => {
+	res.json(posts.filter(post => post.username === req.user.name))
+	
+	
 })
-router.get("/auth", (req, res) => {
-	res.status(200).json(users)
+router.post("/api/login",(req, res) => {
+	
+	
+	//Authenticate User
+	const username = req.body.username
+	const user = {name: username}
+	
+	const accesstoken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+	res.json({accessToken: accesstoken})
 })
+
+
+
+
+
+function authenticateToken(req, res, next){
+	const authHeader = req.headers['authorization']
+	const token = authHeader && authHeader.split(' ')[1]
+	
+	if(token == null) return res.sendStatus(401)
+	
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) =>{
+		if(err) return res.sendStatus(403)
+		req.user = user
+		next()
+	})
+	
+	
+}
 
 module.exports = router;
