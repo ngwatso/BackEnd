ee2e126afdad3ad25265eac23cdea74d8c035cfe
Liliana Leyva <ingeleyva@yahoo.com>
2021-06-20 02:22:44 -0500

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const server = require("./server")
 
-const port = process.env.PORT
+const port = process.env.PORT || 4000
 
 server.listen(port, () => {
 	console.log(`API running at http://localhost:${port}`)
