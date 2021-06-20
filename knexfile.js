b8f0449fd12ec82a3ed880202f9e5ad59b72794a
Liliana Leyva <ingeleyva@yahoo.com>
2021-06-20 03:03:16 -0500

    ..

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,46 @@
 // do not make changes to this file
-const sharedConfig = {
-  client: 'sqlite3',
-  useNullAsDefault: true,
-  migrations: { directory: './data/migrations' },
-  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
-}
+// const sharedConfig = {
+//   client: 'sqlite3',
+//   useNullAsDefault: true,
+//   migrations: { directory: './data/migrations' },
+//   pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+// }
 
+// module.exports = {
+//   development: {
+//     ...sharedConfig,
+//     connection: { filename: './data/usersdb.db3' },
+//     seeds: { directory: './data/seeds' },
+//   },
+//   // testing: {
+//   //   ...sharedConfig,
+//   //   connection: { filename: './data/test.db3' },
+//   // }, //
+// };
+require("dotenv").config();
 module.exports = {
   development: {
-    ...sharedConfig,
-    connection: { filename: './data/usersdb.db3' },
-    seeds: { directory: './data/seeds' },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    useNullAsDefault: true,
+    debug: true
   },
-  // testing: {
-  //   ...sharedConfig,
-  //   connection: { filename: './data/test.db3' },
-  // }, //
+  production: {
+    client: "pg",
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    useNullAsDefault: true,
+    debug: true
+  }
 };
